36162ef099dbff5b4ade088e46ef8369d0a0001b
castroco <carlos.castro.p@gmail.com>
2021-04-24 20:59:35 -0400

    commit

--- js/assignment.js
+++ js/assignment.js
@@ -3,9 +3,16 @@ function addUpdatedDate() {
     try {
         var dateUpdated = document.lastModified;
         const pObj = document.querySelector('#lastUpdated');
-        message = `Last updated ${dateUpdated}`;
+        let message = `Last updated ${dateUpdated}`;
         console.log(message);
         pObj.innerHTML = message;
+
+        //geting the full year
+        let day = new Date();
+        let fullYear = day.getFullYear();
+        const pObj2 = document.querySelector('#fullYear');
+        let message2 = `&#169; | Carlos Oliver Castro | Santiago | <a href= "https://www.byui.edu/online">BYU Online Learning</a>`
+        pObj2.innerHTML = message2;
     }
         catch(err) {
         alert(err.message);
