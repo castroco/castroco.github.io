3c1387c755f949ed426469e93dc89abde0c2e3eb
castroco <carlos.castro.p@gmail.com>
2021-05-21 10:37:12 -0400

    commit

---
+++ lesson5/js/bom.js
@@ -0,0 +1,28 @@
+
+  /*
+  let li = document.createElement('____');
+  deleteButton.addEventListener('click', function() {
+    // this line of code should use the removeChild() (Links to an external site.) method
+    // this line of code should move the cursor back to the input box using the focus (Links to an external site.) method. 
+  });
+
+  const sect = document.querySelector('section');
+
+  const para = document.createElement('p');
+  para.textContent = 'We hope you enjoyed the ride.';
+
+  sect.appendChild(para);
+
+  const text = document.createTextNode(' — the premier source for web development knowledge.');
+
+  const linkPara = document.querySelector('p');
+  linkPara.appendChild(text);
+
+  Node.cloneNode()
+
+  sect.removeChild(linkPara);
+  linkPara.remove();
+
+  linkPara.parentNode.removeChild(linkPara); /* older browser  */
+
+
