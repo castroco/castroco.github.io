1a5a2076e9e7e1cc0d9307818dd3fdc5090f33a8
castroco <carlos.castro.p@gmail.com>
2021-07-02 22:37:40 -0400

    commit

--- lesson-11/js/gallery.js
+++ lesson-11/js/gallery.js
@@ -61,29 +61,6 @@ if('IntersectionObserver' in window) {
  
 }
 
-const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=10d779ad423d396cdcffa6696f58adce&units=imperial';
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=10d779ad423d396cdcffa6696f58adce&units=imperial';
-
-fetch(apiURL)
-    .then((response) => response.json())
-    .then((jsObject) => {
-            /*console.log(jsObject);*/
-            const temperature = jsObject.main.temp;
-            document.getElementById('temperature').textContent = temperature;
-            const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
-            const desc = jsObject.weather[0].description;
-            const winspeed = jsObject.wind.speed;
-            const humidity = jsObject.main.humidity;
-            document.getElementById('currently').textContent = desc;
-            /*document.getElementById('icon').setAttribute('src', imagesrc);
-            document.getElementById('icon').setAttribute('alt', desc);*/
-            document.getElementById('windspeed').textContent = winspeed;
-            document.getElementById('humidity').textContent = humidity;
-            const windchill = windChill(temperature, winspeed);
-            document.getElementById('windchill').textContent = windchill;
-    }
-);
-
 function windChill(tempF, speed) {
     /*tempF = 40;*/
     let factor = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * tempF * Math.pow(speed, 0.16));
